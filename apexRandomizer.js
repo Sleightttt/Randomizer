7c154a8f25890dbd007daf24b6ac2bc8a933a66e
Brennan Cota <brennancota@yahoo.com>
2023-04-21 10:16:48 -0700

    plan settled

--- apexRandomizer.js
+++ apexRandomizer.js
@@ -575,3 +575,4 @@ const roll = document
   });
 //move leegend classes to seperate file
 //learn how to use require properly to set up seperate file for classes to be held in
+//refactor entire project to flask/react
